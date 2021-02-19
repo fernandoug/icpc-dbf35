3ce84e8ac28e509ab647148e644a6f2dfb585dd6
Stackbit Code Editor <support@stackbit.com>
2021-02-19 22:11:37 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ export default class HeroSection extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         let background = _.get(section, 'background', null);
-        //let background_color = _.get(background, 'background_color', null) || 'white';
+        let background_color = _.get(background, 'background_color', null) || 'white';
         let background_opacity_pct = _.get(background, 'background_image_opacity', null) || 100;
         let background_opacity = background_opacity_pct * 0.01;
         let background_size = _.get(background, 'background_image_size', null) || 'cover';
